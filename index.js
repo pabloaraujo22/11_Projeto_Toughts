dce31f0758ab0f31a65c8e8e7e19903bc272bbb0
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-01-13 20:16:25 -0300

    2_Finalizando Estrutur

--- index.js
+++ index.js
@@ -8,6 +8,46 @@ const app = express()
 
 const conn = require('./db/conn')
 
+//template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+//receber resposta do body
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//session middleware
+app.use(session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false, //false - caiu a sessao ele desconecta
+    saveUninitialized: false,
+    store: new FileStore({
+        logFn: function() {},
+        path: require('path').join(require('os').tmpdir(), 'session') //caminho temporario para acessar sessao
+    }),
+    cookie: {
+        secure: false,
+        maxAge: 360000, //deixa de ser valido
+        expires: new Date(Date.now() + 360000), //expira autamaticamente
+        httpOnly: true, //true - por usar http e nao https SSL
+    }
+}))
+
+//flash messages
+app.use(flash())
+
+//public path
+app.use(express.static('public'))
+
+//set session to res
+app.use((req, res, next) => {
+    if (req.session.userid) {
+        res.locals.session = req.session
+    }
+
+    next()
+})
 
 conn.sync()
     .then(() => app.listen(3000))
