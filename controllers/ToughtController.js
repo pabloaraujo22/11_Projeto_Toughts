cefd45d1c1b8a0aa9a26b567923ef2a2c8f2e262
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-01-17 16:43:27 -0300

    Inserindo Pensamentos no Banco

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -9,4 +9,28 @@ module.exports = class ToughtController {
     static async dashboard(req, res) {
         res.render('toughts/dashboard')
     }
+
+    static async createTought(req, res) {
+        res.render('toughts/create')
+    }
+
+    static async createToughtSave(req, res) {
+        const title = req.body.title
+        const UserId = req.session.userid
+        if (!UserId) {
+            return res.redirect('/login')
+        }
+
+        try {
+            const tought = { title, UserId }
+            await Tought.create(tought)
+            req.flash('message', 'Pensamento criado com Sucesso!')
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (erro) {
+            console.log(erro)
+        }
+
+    }
 }
